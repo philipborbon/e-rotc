a0c428eb556e7473c6e25a90e2139628baf3fd66
Philip Borbon <philipjohnborbon@gmail.com>
2020-05-27 19:49:23 +0800

    Assessment for military correspondence

--- app/src/main/java/com/erotc/learning/database/Database.kt
+++ app/src/main/java/com/erotc/learning/database/Database.kt
@@ -14,7 +14,7 @@ import com.erotc.learning.data.*
     Tutorial::class,
     Assessment::class,
     Leaderboard::class
-], version = 6)
+], version = 7)
 @TypeConverters(Converter::class)
 abstract class Database : RoomDatabase() {
     abstract fun topicDao(): TopicDao
