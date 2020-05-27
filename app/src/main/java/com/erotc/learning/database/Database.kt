91f7b27bdbdba6d52d5d993dd36d82aaf422e90a
Philip Borbon <philipjohnborbon@gmail.com>
2020-05-28 03:24:02 +0800

    Assessment for rifle

--- app/src/main/java/com/erotc/learning/database/Database.kt
+++ app/src/main/java/com/erotc/learning/database/Database.kt
@@ -14,7 +14,7 @@ import com.erotc.learning.data.*
     Tutorial::class,
     Assessment::class,
     Leaderboard::class
-], version = 17)
+], version = 18)
 @TypeConverters(Converter::class)
 abstract class Database : RoomDatabase() {
     abstract fun topicDao(): TopicDao
