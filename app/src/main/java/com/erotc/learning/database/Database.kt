66fd4256cdc7c4e38e365d19ce9e4e0913509625
Philip Borbon <philipjohnborbon@gmail.com>
2020-05-27 19:20:26 +0800

    Assessment for mcd

--- app/src/main/java/com/erotc/learning/database/Database.kt
+++ app/src/main/java/com/erotc/learning/database/Database.kt
@@ -14,7 +14,7 @@ import com.erotc.learning.data.*
     Tutorial::class,
     Assessment::class,
     Leaderboard::class
-], version = 5)
+], version = 6)
 @TypeConverters(Converter::class)
 abstract class Database : RoomDatabase() {
     abstract fun topicDao(): TopicDao
