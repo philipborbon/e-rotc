ec377dd4b011346f612e77d7d777d131b20ba504
Philip Borbon <philipjohnborbon@gmail.com>
2020-05-28 01:17:14 +0800

    Assessment for basic intelligence

--- app/src/main/java/com/erotc/learning/database/Database.kt
+++ app/src/main/java/com/erotc/learning/database/Database.kt
@@ -14,7 +14,7 @@ import com.erotc.learning.data.*
     Tutorial::class,
     Assessment::class,
     Leaderboard::class
-], version = 12)
+], version = 13)
 @TypeConverters(Converter::class)
 abstract class Database : RoomDatabase() {
     abstract fun topicDao(): TopicDao
