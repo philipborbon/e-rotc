65e076755b13ddb361ac9cd335dd1d98d5b7f819
Philip Borbon <philipjohnborbon@gmail.com>
2020-05-27 16:07:30 +0800

    Assessment for legal basis

--- app/src/main/java/com/erotc/learning/database/Database.kt
+++ app/src/main/java/com/erotc/learning/database/Database.kt
@@ -14,7 +14,7 @@ import com.erotc.learning.data.*
     Tutorial::class,
     Assessment::class,
     Leaderboard::class
-], version = 2)
+], version = 3)
 @TypeConverters(Converter::class)
 abstract class Database : RoomDatabase() {
     abstract fun topicDao(): TopicDao
