5abac21bd6120cad93b1ab9fccca1dd5ee0b757e
Philip Borbon <philipjohnborbon@gmail.com>
2020-05-27 22:34:06 +0800

    Assessment for first aid

--- app/src/main/java/com/erotc/learning/database/Database.kt
+++ app/src/main/java/com/erotc/learning/database/Database.kt
@@ -14,7 +14,7 @@ import com.erotc.learning.data.*
     Tutorial::class,
     Assessment::class,
     Leaderboard::class
-], version = 9)
+], version = 10)
 @TypeConverters(Converter::class)
 abstract class Database : RoomDatabase() {
     abstract fun topicDao(): TopicDao
