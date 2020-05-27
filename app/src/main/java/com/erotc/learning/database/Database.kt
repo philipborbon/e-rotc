128c67c22839dd6c5dd251cad076ea39806c2d0c
Philip Borbon <philipjohnborbon@gmail.com>
2020-05-27 23:18:15 +0800

    Assessment for marksmanship

--- app/src/main/java/com/erotc/learning/database/Database.kt
+++ app/src/main/java/com/erotc/learning/database/Database.kt
@@ -14,7 +14,7 @@ import com.erotc.learning.data.*
     Tutorial::class,
     Assessment::class,
     Leaderboard::class
-], version = 10)
+], version = 11)
 @TypeConverters(Converter::class)
 abstract class Database : RoomDatabase() {
     abstract fun topicDao(): TopicDao
