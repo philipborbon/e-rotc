52b3bcf982c5e7c3619e80972f4cb3b60333f5c6
Philip Borbon <philipjohnborbon@gmail.com>
2020-05-28 02:00:23 +0800

    Assessment for hygiene and sanitation

--- app/src/main/java/com/erotc/learning/database/Database.kt
+++ app/src/main/java/com/erotc/learning/database/Database.kt
@@ -14,7 +14,7 @@ import com.erotc.learning.data.*
     Tutorial::class,
     Assessment::class,
     Leaderboard::class
-], version = 14)
+], version = 15)
 @TypeConverters(Converter::class)
 abstract class Database : RoomDatabase() {
     abstract fun topicDao(): TopicDao
